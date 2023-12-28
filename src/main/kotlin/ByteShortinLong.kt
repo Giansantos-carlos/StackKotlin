cd71418129af569009972141324ec9c5c0973cf4
Gian Carlos <giansantos.carlos@gmail.com>
2023-12-27 22:47:00 -0300

    ByteShortinLOng

---
+++ src/main/kotlin/ByteShortinLong.kt
@@ -0,0 +1,31 @@
+
+fun main() {
+
+    var number: Int = 22
+
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    val minIntegerValue: Int = Int.MIN_VALUE
+
+    println(maxIntegerValue)
+    println(minIntegerValue)
+
+    number = 2147483647
+
+    val maxByteValue: Byte = Byte.MAX_VALUE
+    val minByteValue: Byte = Byte.MIN_VALUE
+
+    println(maxByteValue)
+    println(minByteValue)
+
+    val maxShortValue: Short =  Short.MAX_VALUE
+    val minShortValue: Short =  Short.MIN_VALUE
+
+    println(maxShortValue)
+    println(minShortValue)
+
+    val maxLongValue: Long = Long.MAX_VALUE
+    val minLongValue: Long = Long.MIN_VALUE
+
+    println(maxLongValue)
+    println(minLongValue)
+}
